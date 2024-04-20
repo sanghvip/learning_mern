2e7b3b7e394f38ccbc277e1d6e6e5ef4edd5d817
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-04-20 23:51:55 +0000

    adding layout components

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+
+const Header = ()=> {
+    return (
+        <div className="bg-blue-800 py-6">
+            <div className="container mx-auto flex justify-between">
+                <span className="text-3xl text-white font-bold tracking-tight">
+                    <Link></Link>
+                </span>
+                <span>
+
+                </span>
+
+            </div>
+
+
+        </div>
+    )
+}
