df3c3a1646fd5e19faf448e36c7a799561794ce7
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-04-22 23:36:55 +0000

    adding firstname and last name to our app

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,7 +1,27 @@
+import { useForm} from "react-hook-form";
+type RegisterFormData = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+}
 const Register = () => {
+    const {register} = useForm<RegisterFormData>();
+
     return (
         <form className="flex flex-col gap-5">
             <h2 className="text-3xl font-bold">Create an account</h2>
+            <div className="flex flex-col md:flex-row gap-5">
+                <label className="text-gray-700 text-sm font-bold flex-1">
+                    FirstName
+                    <input className="border rounded w-full py-1 px-2 font-normal" {...register("firstName", { required: "This field is required" })}></input>
+                </label> 
+                <label className="text-gray-700 text-sm font-bold flex-1" >
+                    LastName
+                    <input className="border rounded w-full py-1 px-2 font-normal" {...register("lastName", { required: "This field is required"})}></input>
+                </label>
+            </div>
         </form>
     )
 };
