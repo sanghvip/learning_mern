7d76820b8d5aacac3a38c266200e659a218e5c26
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-04-22 00:03:07 +0000

    adding new connection string and updating model attribute names

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -22,7 +22,7 @@ router.post("/register", async (req: Request, res: Response)=>{
 
         const token = jwt.sign({userId: user.id},process.env.JWT_SECRET_KEY as string,{
             expiresIn: "1d",
-        })
+        });
         res.cookie("auth_token",token, {
             httpOnly: true,
             secure: process.env.NODE_ENV==="production",
