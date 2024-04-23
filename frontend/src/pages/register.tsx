ab7ec8063553f73d76a41ed066ead35f8c05239b
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-04-23 00:01:44 +0000

    adding error message display logic to our form

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -7,7 +7,7 @@ type RegisterFormData = {
     confirmPassword: string
 }
 const Register = () => {
-    const {register,watch,handleSubmit} = useForm<RegisterFormData>();
+    const {register,watch,handleSubmit,formState:{errors}} = useForm<RegisterFormData>();
     const onSubmit = handleSubmit((data) => {
         console.log(data);
     });
@@ -18,19 +18,31 @@ const Register = () => {
                 <label className="text-gray-700 text-sm font-bold flex-1">
                     FirstName
                     <input className="border rounded w-full py-1 px-2 font-normal" {...register("firstName", { required: "This field is required" })}></input>
+                    {errors.firstName && (
+                        <span className="text-red-500">{errors.firstName.message}</span>
+                    )}
                 </label> 
                 <label className="text-gray-700 text-sm font-bold flex-1" >
                     LastName
                     <input className="border rounded w-full py-1 px-2 font-normal" {...register("lastName", { required: "This field is required"})}></input>
+                    {errors.lastName && (
+                        <span className="text-red-500">{errors.firstName.message}</span>
+                    )}
                 </label>
             </div>
             <label className="text-gray-700 text-sm font-bold flex-1" >
             Email
                 <input type="email" className="border rounded w-full py-1 px-2 font-normal" {...register("email", { required: "This field is required"})}></input>
+                {errors.email && (
+                        <span className="text-red-500">{errors.email.message}</span>
+                    )}
             </label>
             <label className="text-gray-700 text-sm font-bold flex-1" >
             Password
                 <input type="email" className="border rounded w-full py-1 px-2 font-normal" {...register("password", { required: "This field is required", minLength: { value: 6, message: "Password must be atleast 6 characters" } })}></input>
+                {errors.password && (
+                        <span className="text-red-500">{errors.password.message}</span>
+                    )}
             </label>
             <label className="text-gray-700 text-sm font-bold flex-1" >
             Confirm Password
@@ -45,6 +57,9 @@ const Register = () => {
                     }
                 })
             }>
+                {errors.confirmPassword && (
+                        <span className="text-red-500">{errors.confirmPassword.message}</span>
+                    )}
             </input>
             </label>
             <span>
