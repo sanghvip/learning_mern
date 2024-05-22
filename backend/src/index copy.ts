cf8e4ef9481072001ab88c2ab346ff45d2a303cf
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-05-22 11:57:58 +0000

    Added dbconnection test code

---
+++ backend/src/index copy.ts
@@ -0,0 +1,32 @@
+import express, {Request,Response, application} from'express';
+import cors from 'cors';
+//The Dotenv/config import loads your environment variables
+import "dotenv/config";
+//Mongoos epackage helps us to connect with our database
+import mongoose, { mongo } from "mongoose";
+import userRoutes  from './routes/users';
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+console.log("Connection String:"+(process.env.MONGODB_CONNECTION_STRING)as string);
+
+const app = express();
+app.use(cookieParser());
+
+//Helps to convert API request body to json automatically
+app.use(express.json());
+// Helps to parse the URL to get the create parameters
+app.use(express.urlencoded({extended:true}));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials:true,
+}));
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users", userRoutes);
+
+//start the server for supporing the requests
+app.listen(7000,() => {
+    console.log("Server is running on localhost:7000");
+})
